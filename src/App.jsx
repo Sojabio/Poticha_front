341bb2223dddc9142056b12a47a81f01b66d2e3e
soja <solene.jabaud@gmail.com>
2023-09-12 16:21:22 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,9 @@ import Info from './pages/dynamics/Info';
 import Login from './pages/Admin/Auth/Login';
 import Dashboard from './pages/Admin/Dashboard';
 import LogoutSuccess from './pages/Admin/Auth/LogoutSuccess';
+import UpdatePost from './components/Admin/Infos/update';
+import UpdateAuthor from './components/Admin/Authors/update';
+
 
 
 function App() {
@@ -69,6 +72,9 @@ function App() {
           <Route path="/login" element={<Login/>} />
           <Route path="/admin" element={<Dashboard/>} />
           <Route path="/logoutsuccess" element={<LogoutSuccess/>} />
+          <Route path="/updatepost/:id" element={<UpdatePost/>} />
+          <Route path="/updateauthor/:id" element={<UpdateAuthor/>} />
+
 
 
         </Routes>
