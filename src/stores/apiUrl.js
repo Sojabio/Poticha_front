a1dab1b366be8cc8247023e9f0cbcaa55b78e470
soja <solene.jabaud@gmail.com>
2023-09-15 09:59:17 +0200

    test deploy

--- src/stores/apiUrl.js
+++ src/stores/apiUrl.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://127.0.0.1:3000";
-// export const API_URL = "https://poticha-8b152725d841.herokuapp.com/"
+// export const API_URL = "http://127.0.0.1:3000";
+export const API_URL = "https://poticha-8b152725d841.herokuapp.com/"
