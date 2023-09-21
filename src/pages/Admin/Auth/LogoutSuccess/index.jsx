d32ef9cf2a2fe30312527576f7605bab9b75414f
soja <solene.jabaud@gmail.com>
2023-09-21 14:29:20 +0200

    logout success

--- src/pages/Admin/Auth/LogoutSuccess/index.jsx
+++ src/pages/Admin/Auth/LogoutSuccess/index.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import './style.css'
+import { Link } from 'react-router-dom';
 
 const LogoutSuccess = () => {
+
   return (
-    <div>Bravi bravo LogoutSuccess</div>
+    <div className="logout">
+      <div className="logout-container">
+        <p> Déconnexion réussie ! </p>
+        <Link  to= '/' className="link-home">
+          <div className="home-button">Retour à la page d'accueil</div>
+        </Link>
+      </div>
+    </div>
   )
 }
 
